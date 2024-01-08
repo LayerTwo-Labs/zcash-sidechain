29fbde10656f8f19b297e0ca619e40955628107e
Torkel Rogstad <torkel@rogstad.io>
2024-01-08 11:09:18 +0100

    depends: replace Boost URL
    Their Jfrog repo is having issues.

--- depends/packages/boost.mk
+++ depends/packages/boost.mk
@@ -1,6 +1,6 @@
 package=boost
 $(package)_version=1_79_0
-$(package)_download_path=https://boostorg.jfrog.io/artifactory/main/release/$(subst _,.,$($(package)_version))/source/
+$(package)_download_path=https://archives.boost.io/release/$(subst _,.,$($(package)_version))/source/
 $(package)_file_name=boost_$($(package)_version).tar.bz2
 $(package)_sha256_hash=475d589d51a7f8b3ba2ba4eda022b170e562ca3b760ee922c146b6c65856ef39
 $(package)_dependencies=native_b2
