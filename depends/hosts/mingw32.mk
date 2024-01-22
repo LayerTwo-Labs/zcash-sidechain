69dff084def9a35133d5edfe4a3881cbd8b7706f
Torkel Rogstad <torkel@rogstad.io>
2024-01-22 14:34:55 +0100

    depends: backport zcash #6517

--- depends/hosts/mingw32.mk
+++ depends/hosts/mingw32.mk
@@ -3,6 +3,9 @@ mingw32_CXXFLAGS=$(mingw32_CFLAGS) -isystem $(host_prefix)/include/c++/v1
 
 mingw32_LDFLAGS?=-fuse-ld=lld
 
+# Port from upstream https://github.com/zcash/zcash/commit/c3693a5f857b27224707538179840eea223c1b41
+mingw32_LDFLAGS+=-L/usr/lib/gcc/x86_64-w64-mingw32/$(shell x86_64-w64-mingw32-g++-posix -dumpversion)
+
 mingw32_release_CFLAGS=-O3
 mingw32_release_CXXFLAGS=$(mingw32_release_CFLAGS)
 
